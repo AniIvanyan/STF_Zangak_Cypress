13dee9eb0a0735ec36696c1fe5042d3be8d22c21
Ani Ivanyan <aniivanyan23@gmail.com>
2024-05-07 00:22:17 +0400

    Update .gitignore and ignore tracked files

--- cypress/pom/search/searchAssertions.js
+++
@@ -1,7 +0,0 @@
-class SearchAssertions {
-    isNoResultsMessageVisible() {
-        return cy.contains('Ոչինչ չի գտնվել, փորձիր նորից').should('be.visible');
-    }
-}
-
-export default new SearchAssertions();
--- cypress/pom/search/searchLocators.js
+++
@@ -1,7 +0,0 @@
-class SearchLocator {
-    getSearchInput() {
-        return cy.get('input.form-control.form-control-lg.search-string[name="q"][placeholder="Որոնել"]');
-    }
-}
-
-export default new SearchLocator();
